c64557b2424ee358b90bd75575584ee6e068cc04
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-19 18:44:06 +0200

    add model for sotring urls

--- backend/url_trimmer/models.py
+++ backend/url_trimmer/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class SaveURL(models.Model):
+    url = models.URLField(max_length=300)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='urls')
+
+    def __str__(self):
+        return str(self.url)
