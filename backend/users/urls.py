6b183222778b4d7b36987122a00aa0d3cd2544fe
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-19 09:50:05 +0200

    add registration for users

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
-# from django.urls import path
-#
-# from . import views
-#
-# app_name = 'users'
-#
-# urlpatterns = [
-#     path('', )
-# ]
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login', views.login_user_view, name='login'),
+    path('register', views.registration_view, name='register'),
+]
