1d206cbb5ca08be17072763dd7219f1c2601820a
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-19 20:58:25 +0200

    add alias, created_at and ordering for SaveURL

--- backend/url_trimmer/models.py
+++ backend/url_trimmer/models.py
@@ -4,7 +4,12 @@ from django.db import models
 
 class SaveURL(models.Model):
     url = models.URLField(max_length=300)
+    alias = models.URLField(max_length=100, default=0, unique=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='urls')
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created_at', )
 
     def __str__(self):
         return str(self.url)
