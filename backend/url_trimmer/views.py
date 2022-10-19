d557e7cc45bad3fa2bc03f0c4be85f435eb76964
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-19 18:47:34 +0200

    add view and template for addin new url

--- backend/url_trimmer/views.py
+++ backend/url_trimmer/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import forms
+
+
+def add_new_url(request):
+    form = forms.AddUrl(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            url = form.save(commit=False)
+            url.user = request.user
+            url.save()
+            return HttpResponseRedirect(reverse_lazy('home:home'))
+
+    return render(request, 'url_trimmer/add_url.html', {'form': form})
