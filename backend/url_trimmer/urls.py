5150adbbf873e5b8a6c2a3b191b344eea877aee7
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-20 15:57:28 +0200

    add detail view for single url from database

--- backend/url_trimmer/urls.py
+++ backend/url_trimmer/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'url_trimmer'
 
 urlpatterns = [
-
+    path('my-links/<int:link_id>', views.link_detail_view, name='link_detail')
 ]
--- backend/url_trimmer/views.py
+++ backend/url_trimmer/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
-from django.views.generic import FormView
-from django.views.generic.detail import SingleObjectMixin
+
+from django.shortcuts import render, redirect, get_object_or_404
 
 from . import forms, models
+
+
+def link_detail_view(request, link_id):
+    link = get_object_or_404(models.SaveURL, pk=link_id)
+    context = {'link': link}
+
+    return render(request, 'url_trimmer/link_detail.html', context)
 
