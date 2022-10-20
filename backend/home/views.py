8a351ac56f5a881ae5106d173fb6b16095d46749
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-20 18:20:55 +0200

    handle rediracting ANonymusUser to login page when entering homepage

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 
@@ -26,4 +26,4 @@ def home_view(request):
 
         return render(request, 'home/home.html', context)
 
-    return render(request, 'home/home.html')
+    return redirect(reverse_lazy('users:login'))
