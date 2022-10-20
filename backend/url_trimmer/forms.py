20b3deb5f87830b887c853d4459df3d597639989
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-20 22:06:45 +0200

    add documentation

--- backend/url_trimmer/forms.py
+++ backend/url_trimmer/forms.py
@@ -3,7 +3,8 @@ from . import models
 
 
 class AddUrl(forms.ModelForm):
-
+    """Saves url instance to databease.
+    """
     class Meta:
         model = models.SaveURL
         fields = ('url',)
