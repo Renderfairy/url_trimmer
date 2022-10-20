489055fdb1545969130e63f6a10b55e914028822
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-20 18:50:41 +0200

    changed pattern for generating alias for link

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.utils.crypto import get_random_string
 
 
 from url_trimmer import forms, models
@@ -20,7 +21,7 @@ def home_view(request):
             if form.is_valid():
                 url = form.save(commit=False)
                 url.user = request.user
-                url.alias = models.SaveURL.objects.all().count()
+                url.alias = get_random_string(length=10)
                 url.save()
                 return HttpResponseRedirect(reverse_lazy('home:home'))
 
