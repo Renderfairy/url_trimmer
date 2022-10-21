26f933d2ae16ee2501db8c7f9f012e19ddcc52a1
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-21 10:48:25 +0200

    add fixtures for testing

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,10 +1,13 @@
 import pytest
+from django.contrib.auth import get_user_model
+
+from url_trimmer.models import SaveURL
 
 
 @pytest.fixture(scope='function')
 def user(db, django_user_model):
     """
-    User instance from default user model and Address instance from 'users.models'
+    User instance from default user model and Address instance from `users.models`
     that is necessary to create a User object.
     """
 
@@ -14,3 +17,14 @@ def user(db, django_user_model):
         email='office@happyhorses.pl',
     )
     yield user
+
+
+@pytest.fixture(scope='function')
+def new_url(db):
+    user = get_user_model().objects.get(pk=1)
+    new_url = SaveURL.objects.create(
+        url='jetbrains.com',
+        user=user
+    )
+
+    yield new_url
--- backend/home/tests/test_home_views.py
+++ backend/home/tests/test_home_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from url_trimmer.models import SaveURL
 
 
 def test_anonymus_user_redirect_from_home_to_login(client):
@@ -18,3 +19,14 @@ def test_authenticated_user_home_page_access(client, user):
 
     assert response.status_code == 200
     assert '<h2>My links</h2>' in response.content.decode('UTF-8')
+
+
+@pytest.mark.django_db
+def test_add_new_url(client, user):
+    client.force_login(user)
+    url = reverse('home:home')
+    client.post(url, data={
+        'url': 'jetbrains.com/objc/',
+    })
+
+    assert len(SaveURL.objects.all()) == 1
