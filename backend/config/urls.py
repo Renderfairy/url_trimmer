a6bcf0b756ae49e59bfa64a43593678e989950e4
Martyna Witkowska <mar.witkowska@gmail.com>
2022-11-13 17:28:44 +0100

    add drf auth routing

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,20 +17,29 @@ from django.conf.urls.static import static
 from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 from . import settings
-from url_trimmer import views
+from url_trimmer import views as ut_views
+from users import views as u_views
+
+
+router = routers.SimpleRouter()
+router.register(r'users', u_views.UsersList, basename='user_list')
+# router.register('links/', ut_views.ListURLView, basename='urls_list')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("", include("users.urls")),
     path("", include("url_trimmer.urls")),
-    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace='rest_framework')),
 
 ]
 
-handler404 = views.error_404
+handler404 = ut_views.error_404
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
