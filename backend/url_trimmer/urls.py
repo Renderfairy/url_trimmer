58829b64f80bee6d7e717196cc331008ced30b63
Martyna Witkowska <mar.witkowska@gmail.com>
2022-12-16 13:17:28 +0100

    refactor 404 view to return right code in template response

--- backend/url_trimmer/urls.py
+++ backend/url_trimmer/urls.py
@@ -6,6 +6,6 @@ app_name = 'url_trimmer'
 
 urlpatterns = [
     path('my-links/<int:pk>', views.LinkDetailView.as_view(), name='link_detail'),
-    path('<str:alias>', views.link_redirect, name='link_redirect'),
+    path('<str:alias>', views.LinkRedirectView.as_view(), name='link_redirect'),
     path('my-links/<str:alias>/delete', views.LinkDeleteView.as_view(), name='link_delete'),
 ]
--- backend/url_trimmer/views.py
+++ backend/url_trimmer/views.py
@@ -1,44 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404, HttpResponse
+from django.shortcuts import redirect, get_object_or_404
+from django.template.response import TemplateResponse
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, DeleteView
+from django.views import View
+from django.views.generic import DetailView, DeleteView, TemplateView
 
 from .models import URL
-
-
-def link_redirect(request, alias):
-    """
-        Redirect to instances original url :model:`url_trimmer.SaveURL`.
-
-        **Context**
-
-        ``link``
-            An instance of :model:`url_trimmer.SaveURL`.
-
-    """
-    link = get_object_or_404(URL, alias=alias)
-    return redirect(f'{link.url}')
-
-
-def link_delete(request, alias):
-    """
-       Delete instance :model:`url_trimmer.SaveURL`.
-
-       **Context**
-
-       ``link``
-           An instance of :model:`url_trimmer.SaveURL`.
-
-   """
-    link = get_object_or_404(URL, alias=alias)
-    link.delete()
-
-    return redirect(reverse_lazy('home:home'))
-
-
-def error_404(request, exception, template_name='url_trimmer/error_404.html'):
-    return render(request, template_name)
 
 
 class LinkDetailView(LoginRequiredMixin, DetailView):
@@ -62,3 +30,12 @@ class LinkDeleteView(LoginRequiredMixin, DeleteView):
             raise Http404
         return obj
 
+
+class LinkRedirectView(View):
+    def get(self, request, alias):
+        link = get_object_or_404(URL, alias=alias)
+        return redirect(f'{link.url}')
+
+
+def links_404_view(request, exception=None):
+    return TemplateResponse(request=request, template='url_trimmer/error_404.html', status=404)
